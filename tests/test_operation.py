44896c08d3235914a28265ce6296ac2681c0f9b1
sanikacentric <128413702+sanikacentric@users.noreply.github.com>
2025-03-24 16:35:18 -0400

    Update test_operation.py

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,4 +1,10 @@
-from src.math_operations import add, sub
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+from math_operations import add, sub
+
 
 def test_add():
     assert add(2,3)==5
